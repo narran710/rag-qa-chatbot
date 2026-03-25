bd93d440c4d9ab559b4e9fd97a6718db3ec73745
Narran <narran710@gmail.com>
2026-03-25 16:11:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ st.title("🤖 RAG QA Chatbot")
 # -----------------------------
 # API KEY
 # -----------------------------
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY") or st.secrets["GROQ_API_KEY"]
 
 if not GROQ_API_KEY:
     st.error("❌ GROQ API Key not found! Add it in .env file")
